186fb5b494bafd8e41368acfb870fa075da19cda
keshab sutar <keshabsingh981829@gmail.com>
2024-05-07 14:21:32 +0545

    simple LOGIN page

---
+++ nextjs/src/app/page.js
@@ -0,0 +1,12 @@
+const page = () => {
+  return (
+    <div>
+      <p className="login"><b>LOGIN:</b></p>
+      <input placeholder="enter username" /><br></br>
+      <input placeholder="enter password" /><br></br>
+      <button>Click me</button>
+    </div>
+  );
+};
+
+export default page;
