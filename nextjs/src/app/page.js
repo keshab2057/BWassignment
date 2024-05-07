095aa303f1c39103bbd7c5b44c31a00c863a3b6b
keshab sutar <keshabsingh981829@gmail.com>
2024-05-07 15:02:34 +0545

    designing login page

--- nextjs/src/app/page.js
+++ nextjs/src/app/page.js
@@ -1,12 +1,47 @@
-const page = () => {
-  return (
-    <div>
-      <p className="login"><b>LOGIN:</b></p>
-      <input placeholder="enter username" /><br></br>
-      <input placeholder="enter password" /><br></br>
-      <button>Click me</button>
-    </div>
-  );
-};
+import React from "react";
+import {
+  Card,
+  Input,
+  Button,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Divider,
+  Link,
+  Image,
+} from "@nextui-org/react";
 
-export default page;
+export default function App() {
+  return (
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="nextui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">LOGIN</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <Input type="email" label="email" />
+        <Input type="password" label="password" />
+        <Button color="primary">Login</Button>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link
+          isExternal
+          showAnchorIcon
+          href="https://github.com/nextui-org/nextui"
+        >
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+  );
+}
